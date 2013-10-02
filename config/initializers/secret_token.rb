74290f37a73226e88f2f38410edf9374446b39b2
Zach Ferland <zachferland@gmail.com>
2013-10-02 19:20:29 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoopSite::Application.config.secret_key_base = '027e5e6b8fefd708183c917cabea8b8a777f350adb46ceef82dd31b924b748575a0a0d0e9db47f03c0a2de24d5c8e36a0de49484b0b5a6d59b1ccab7311175fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoopSite::Application.config.session_store :cookie_store, key: '_CoopSite_session'
